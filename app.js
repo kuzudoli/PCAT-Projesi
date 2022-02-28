d922a10b8ff930eedda8a38082e98651f7d9bac4
kuzudoli <enescakkr@windowslive.com>
2022-02-28 09:10:00 +0300

    Views updated

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const ejs = require("ejs");
 
 const path = require("path");
+
+//Template Engine
+app.set("view engine","ejs");
 
 //Middlewares
 app.use(express.static("public"))
 
 app.get('/',(req,res) => {
-    res.sendFile(path.resolve(__dirname,"temp/index.html"))
+    res.render("index");
+})
+
+app.get('/about',(req,res) => {
+    res.render("about");
+})
+
+app.get('/add',(req,res) => {
+    res.render("add");
 })
 
 app.listen(port, () => {
